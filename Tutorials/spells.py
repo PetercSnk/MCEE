e68daa5baee3bda564191307c4c4b6447beafb4b
PetercSnk <peter.ch.snook@gmail.com>
2023-08-12 10:35:03 +0100

    spells complete

--- Tutorials/spells.py
+++ Tutorials/spells.py
@@ -1,65 +1,42 @@
 # the aim of this class is to use and understand input, processing, and output
 
-# this concept of input-process-output (ipo) is where we take input from the user, process it in some way, and
-# then display the output to the user
+# unfortunately we cannot use the input() command in minecraft
+# use the variables below as your inputs instead
 
-# unfortunately we cannot use the input() command in minecraft, so we will assign our input to a variable
-# this input will be processed where the output will be a command that says the spell in game and moves your pet
-# and places a block
+spell0 = "AlAkAzAm"         # output: "alakazam"
+spell1 = "  baMzook  "      # output: "bamzook"
+spell2 = " AbRa CadabrA  "  # output: "abracadabra"
+spell3 = "abcd123-.,"       # output: "error" 
 
-# taking input from a user can lead to errors when we do not process it correctly
-# people do not think the same and may try different variations of a single input, such as capitals, lowercase,
-# or simply make a mistake
+# process these inputs and say the output in game
+# use the following commands:
 
-# write some code that takes one of the inputs below, processes it by removing whitespace and making it lowercase, 
-# checks if it is a real spell, runs the correct command if it is, otherwise produce error message to user
+# INPUT_STRING.to_lower_case()      # makes lowercase
+# INPUT_STRING.trim()               # removes trailing and leading whitespace
+# INPUT_STRING.replace("X","Y")     # replaces character X with character Y
+# player.say("SAY_IN_GAME")         # says a string in game
 
-# the only real spells are alakazam, bamzook, and abracadabra, anything that doesnt match these after processing
-# should output an error message saying "incorrect"
-
-# inputs
-spell0 = "AlAkAzAm"         # alakazam
-spell1 = "  baMzook  "      # bamzook
-spell2 = " AbRa CadabrA  "  # abracadabra
-spell3 = "abcd123-.,"       # this should tell user it's incorrect, try your own strings to test
-
-# useful commands:
-# INPUT_STRING.to_lower_case()              # makes lowercase
-# INPUT_STRING.trim()                       # removes trailing and leading whitespace
-# INPUT_STRING.replace("x","y")             # replaces character x with character y
-# agent.set_slot(1)                         # sets what slot your pet will use
-# agent.set_item(BLOCK_NAME, COUNT, SLOT)   # sets a slot in your pet to a block
-# agent.move(direction, number_of_blocks)   # moves your pet in a direction, x amount of blocks
-# agent.place(DIRECTION)                    # places the selected slot in a direction
-# directions are UP, DOWN, LEFT, RIGHT, FORWARD, and BACK
-# world(x, y, z)                            # specifies a position in the world
-# agent.teleport(position, direction)       # teleports your pet to a position facing NORTH, SOUTH, EAST or WEST
-# the location you will want your pet to start at is x = -267, y = -43, z = 244, facing north
-
-# alakazam requires BLACKSTONE to be placed 2 blocks from the start
-# bamzook requires ICE to be placed 5 blocks from the start
-# abracadabra requires a DIAMOND_BLOCK to be placed 11 blocks from the start
-
-# speak to Sellen to clear the room and reset your pet 
-
-# to complete this class you will need to place all 3 blocks
-# write your code here
+# write your code here for task 1
 
 
+# once you have managed that we can now command our familiar
+# for each output, a certain block should be placed 
 
+# "alakazam" requires BLACKSTONE, 2 blocks from start
+# "bamzook" requires ICE, 5 blocks from start
+# "abracadabra" requires DIAMOND_BLOCK, 11 blocks from start
 
+# use if-elif-else to compare the processed input to expected output
+# use these commands to control your familiar:
 
+# agent.set_slot(SLOT_NUM)                      # sets active slot to SLOT_NUM
+# agent.set_item(BLOCK_NAME, COUNT, SLOT_NUM)   # puts n blocks in slot SLOT_NUM
+# agent.move(DIRECTION, NUM_BLOCKS)             # moves a direction, x amount of blocks
+# agent.place(DIRECTION)                        # places the active slot in direction
+# DIRECTIONS are UP, DOWN, LEFT, RIGHT, FORWARD, and BACK
+# SLOT_NUM can be 1-27
 
+# talk to Sellen to reset familiar and clear area
 
-
-
-
-
-
-
-
-
-
-
-
+# write your code here for task 2
 
